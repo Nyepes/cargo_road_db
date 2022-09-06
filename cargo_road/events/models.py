e7752399d75c6c86f4349afc265547d9408fa3ff
Nyepes <nicolasyepespulido@gmail.com>
2022-09-06 13:02:46 -0500

    Finished Models: Added Cargo and Driver tables

--- cargo_road/events/models.py
+++ cargo_road/events/models.py
@@ -34,3 +34,34 @@ class DriverSettlement (models.Model):
 	settlement_drivers = models.PositiveIntegerField()
 	pay_date = models.DateField()
 
+class Cargo (models.Model):
+	shipment = models.PositiveIntegerField()
+	truck = models.ForeignKey(Truck, on_delete = models.CASCADE) #hablar con papi sobre on delete
+	driver_settlement = models.ForeignKey(DriverSettlement, on_delete = models.CASCADE)
+	fedex_settlement = models.ForeignKey(FedexSettlement, on_delete = models.CASCADE)
+	description = models.CharField(max_length = 300) #Change max length
+	pickup_date = models.DateField()
+	delivery_date = models.DateField(blank = True)
+	load_miles = models.PositiveSmallIntegerField()
+	loaded_income = models.DecimalField(decimal_places = 2, max_digits = 10)
+	fuel_surcharge = models.DecimalField(decimal_places = 2, max_digits = 10, default = 0)
+	tolls_surcharge = models.DecimalField(decimal_places = 2, max_digits = 10, default = 0)
+	dhpu = models.DecimalField(decimal_places = 2, max_digits = 10, default = 0)
+	empty_income = models.DecimalField(decimal_places = 2, max_digits = 10, default = 0)
+	t_check_advance = models.DecimalField(decimal_places = 2, max_digits = 10, default = 0)
+	traded_value = models.DecimalField(decimal_places = 2, max_digits = 10, default = 0)
+	detention = models.DecimalField(decimal_places = 2, max_digits = 10, default = 0)
+	state = models.CharField(max_length = 20)
+
+class Driver (models.Model):
+	driver_id = models.CharField(max_length=20)
+	name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	w2 = models.BooleanField()
+	driver_id_fedex = models.PositiveIntegerField()
+	standard_pay = models.PositiveSmallIntegerField()
+
+class DriverXCargo(models.Model):
+	shipment = models.ForeignKey(Cargo, on_delete = models.CASCADE)
+	driver_id = models.ForeignKey(Driver, on_delete = models.CASCADE)
+	percentage = models.PositiveSmallIntegerField()
