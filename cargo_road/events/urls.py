7434556dd784cd42aee89e9144ef794db2a6f8bf
AYepesP <alejandroyepespulido@gmail.com>
2022-09-06 14:30:47 -0500

    navBar and viewCargo
    changed the lables in the navbar.  Added html for add cargo and view cargo.   Made view cargo with good ui.  Links all funcional.

--- cargo_road/events/urls.py
+++ cargo_road/events/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name="home"),
+    path('view-cargo', views.view_cargo, name = "view_cargo"),
+    path('add-cargo', views.add_cargo, name = "add_cargo"),
 ]
