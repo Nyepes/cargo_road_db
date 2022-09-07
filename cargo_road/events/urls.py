922fd5e446e30953ab2b63f77db29439044d55a3
AYepesP <alejandroyepespulido@gmail.com>
2022-09-07 07:09:44 -0500

    added Fedex Settlement From, and html
    por alguna razon cuando me meto muestra unos bullet points muy raros.  Hay que cambiar eso

--- cargo_road/events/urls.py
+++ cargo_road/events/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('view-cargo', views.view_cargo, name = "view_cargo"),
     path('add-cargo', views.add_cargo, name = "add_cargo"),
     path('add-truck', views.add_truck,name = 'add_truck'),
+    path('add-fedex-settlement', views.add_fedex_settlement,name = 'add_fedex_settlement'),
 ]
