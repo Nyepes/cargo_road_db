0d88e7273a98b8638dc475df419153d593cd47cc
Nyepes <nicolasyepespulido@gmail.com>
2022-09-11 17:39:21 -0500

    fixed login

--- cargo_road/members/views.py
+++ cargo_road/members/views.py
@@ -8,9 +8,10 @@ def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        print(username + password)
         user = authenticate(request, username = username, password = password)
         if user is not None:
-            login(user)
+            login(request, user)
             return redirect('home')
         else:
             messages.success(request, "There was an error")
